dbecc18ba7136d1e23bbf489db2d81c9018004a4
Augusto Abreu <augusto.abreu@ever.com.br>
2017-09-27 13:19:24 -0300

    Remove createJSModules from Package java file - RN 0.47 compatibility

--- android/src/main/java/net/jodybrewster/linkedinlogin/RNLinkedinLoginPackage.java
+++ android/src/main/java/net/jodybrewster/linkedinlogin/RNLinkedinLoginPackage.java
@@ -26,7 +26,7 @@ public class RNLinkedinLoginPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
+    // Deprecated in RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
